253b4949112c919f368e50f69d3e9b46105be03b
Alex Petenchea <alex.petenchea@gmail.com>
2025-05-15 21:36:03 +0000

    Enhanced script

--- scripts/core/enumerate_segments.py
+++ scripts/core/enumerate_segments.py
@@ -7,8 +7,8 @@ description: |
 
 import sys
 
-import ida_auto
 import ida_pro
+import ida_segment
 import idaapi
 import idautils
 import idc
@@ -25,18 +25,62 @@ def format_perms(perm):
     ])
 
 
-def main(output):
-    # Wait for the auto-analysis to finish
-    ida_auto.auto_wait()
+def format_type(typ):
+    """
+    Turn the segment type into a string.
+    """
+    result = ""
+    if typ == ida_segment.SEG_NORM:
+        result = "unknown type, no assumptions"
+    elif typ == ida_segment.SEG_XTRN:
+        result = "segment with 'extern' definitions"
+    elif typ == ida_segment.SEG_CODE:
+        result = "code segment"
+    elif typ == ida_segment.SEG_DATA:
+        result = "data segment"
+    elif typ == ida_segment.SEG_IMP:
+        result = "java: implementation segment"
+    elif typ == ida_segment.SEG_GRP:
+        result = "group of segments"
+    elif typ == ida_segment.SEG_NULL:
+        result = "zero-length segment"
+    elif typ == ida_segment.SEG_UNDF:
+        result = "undefined segment type (not used)"
+    elif typ == ida_segment.SEG_BSS:
+        result = "uninitialized segment"
+    elif typ == ida_segment.SEG_ABSSYM:
+        result = "segment with definitions of absolute symbols"
+    elif typ == ida_segment.SEG_COMM:
+        result = "segment with communal definitions"
+    elif typ == ida_segment.SEG_IMEM:
+        result = "internal processor memory & sfr (8051)"
+    return result
 
-    # Iterate over all segments
+
+def main(output):
+    # Iterate over all segments.
+    # It enumerates the starting address of each segment, in ascending order.
     for ea in idautils.Segments():
-        seg = idaapi.getseg(ea)
-        name = idc.get_segm_name(ea)
+        # Get the segment object associated with the address.
+        seg: ida_segment.segment_t = ida_segment.getseg(ea)
+
+        # The name property is just an index in a global array of names.
+        # You need to use the get_segm_name function to get the actual name.
+        name = ida_segment.get_segm_name(seg)
+
+        # Each segment has its [start, end) addresses and represents
+        # a contiguous range. The end address is excluded from the segment.
         start = seg.start_ea
         end = seg.end_ea
+
+        # Permissions and type.
         perms = format_perms(seg.perm)
-        print(f"{name}: {hex(start)} - {hex(end)} ({perms})", file=output)
+        typ = format_type(seg.type)
+
+        print(
+            f"{name}: {hex(start)} - {hex(end)} ({perms}) {typ}",
+            file=output
+        )
 
 
 if __name__ == "__main__":
